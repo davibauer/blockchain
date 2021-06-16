7c8b5399872633e82508360d09b7fbde0307b9f1
Davi Bauer <davibauer@gmail.com>
2021-06-16 13:14:56 -0300

    first commit

---
+++ nethereum/NethereumSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nethereum.Web3;
+
+namespace NethereumSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAccountBalance().Wait();
+            Console.ReadLine();
+        }
+
+        static async Task GetAccountBalance()
+        {
+            var web3 = new Web3("https://ropsten.infura.io/v3/39180531508b4b659780ef7a36426a86");
+            var balance = await web3.Eth.GetBalance.SendRequestAsync("0x03d1b3162DBFaB4A175038eAa4EA4b39423d5A6F");
+            Console.WriteLine($"Balance in Wei: {balance.Value}");
+
+            var etherAmount = Web3.Convert.FromWei(balance.Value);
+            Console.WriteLine($"Balance in Ether: {etherAmount}");
+        }
+    }
+}
